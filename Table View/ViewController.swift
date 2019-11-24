975372b108c12519f96eba38b57e7f3e6e55c1e4
Fahim Rahman <44144266+fahimrahmanbooom@users.noreply.github.com>
2019-11-24 16:38:24 +0600

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Fahim Rahman on 24/11/19.
+//  Copyright © 2019 Fahim Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
